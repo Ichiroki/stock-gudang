eda104b7154ee6c49e5fa24b200452b938531bc6
Ichiroki <fahrezirizqiawan12649@gmail.com>
2025-06-27 16:10:21 +0700

    Update: data type for ProdukType

--- resources/js/types/Kategori.ts
+++ resources/js/types/Kategori.ts
@@ -3,7 +3,11 @@ import { ReactNode } from "react"
 export type KategoriType = {
     id: number
     name: string
-}[]
+    categories: {
+        id: number
+        name: string
+    }[]
+}
 
 export interface Kategori {
     name: ReactNode
